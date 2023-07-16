ff4b4a1661a5a160a6c9e053343d46fd697cef7f
olufsenglo <olsheaolufsen@gmail.com>
2023-07-16 06:15:43 +0100

    Added user reducer

---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,18 @@
+// sign up reducer
+export const userReducerSignUp = (state = {}, action) => {
+    switch (action.type) {
+        case USER_SIGNUP_REQUEST:
+            return { loading: true }
+        case USER_SIGNUP_SUCCESS:
+            return {
+                loading: false,
+                userSignUp: action.payload,
+            }
+        case USER_SIGNUP_FAIL:
+            return { loading: false, error: action.payload }
+        case USER_SIGNUP_RESET:
+            return {}
+        default:
+            return state;
+    }
+}
